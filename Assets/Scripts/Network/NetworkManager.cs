d18334a24302106d7015e8d987567f641bfefc76
nicorm2000 <nicorm2000@gmail.com>
2024-05-05 14:49:41 -0300

    Updated Network Manager

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -8,12 +8,26 @@ public struct Client
     public float timeStamp;
     public int id;
     public IPEndPoint ipEndPoint;
+    public string clientName;
 
-    public Client(IPEndPoint ipEndPoint, int id, float timeStamp)
+    public Client(IPEndPoint ipEndPoint, int id, float timeStamp, string clientName)
     {
         this.timeStamp = timeStamp;
         this.id = id;
         this.ipEndPoint = ipEndPoint;
+        this.clientName = clientName;
+    }
+}
+
+public struct Player
+{
+    public int id;
+    public string name;
+
+    public Player(int id, string name)
+    {
+        this.id = id;
+        this.name = name;
     }
 }
 
@@ -36,17 +50,20 @@ public class NetworkManager : MonoBehaviourSingleton<NetworkManager>, IReceiveDa
 
     public int TimeOut = 30;
 
-    public Action<byte[], IPEndPoint> OnReceiveEvent;
-
     private UdpConnection connection;
 
-    private readonly Dictionary<int, Client> clients = new Dictionary<int, Client>();
-    private readonly Dictionary<IPEndPoint, int> ipToId = new Dictionary<IPEndPoint, int>();
+    public readonly Dictionary<int, Client> clients = new(); // Server has this list
+    private readonly Dictionary<int, Player> players = new(); // Clients have this list
+    private readonly Dictionary<IPEndPoint, int> ipToId = new();
 
-    public int serverClientID = 0; // ID that the server assigns to the clients that enter
-    int actualClientID = 0; // The ID of the current client, NOT server
+    public string userName = "Server";
+    public int serverClientId = 0; // ID that the server assigns to new clients
+    public int actualClientId = 0; // ID of this client, not server
 
-    private MessageChecker messageChecker;
+    MessageChecker messageChecker;
+    PingPong checkActivity;
+
+    int maxPlayersPerServer = 4;
 
     private void Start()
     {
@@ -58,49 +75,65 @@ public class NetworkManager : MonoBehaviourSingleton<NetworkManager>, IReceiveDa
         isServer = true;
         this.port = port;
         connection = new UdpConnection(port, this);
+        checkActivity = new PingPong();
     }
 
-    public void StartClient(IPAddress ip, int port)
+    public void StartClient(IPAddress ip, int port, string name)
     {
         isServer = false;
 
         this.port = port;
         this.ipAddress = ip;
+        this.userName = name;
 
         connection = new UdpConnection(ip, port, this);
+        checkActivity = new PingPong();
 
-        NetHandShake netHandShake = new NetHandShake((UdpConnection.IPToLong(ip), port));
-        SendToServer(netHandShake.Serialize());
-        //AddClient(new IPEndPoint(ip, port));
+        ClientToServerNetHandShake handShakeMesage = new((UdpConnection.IPToLong(ip), port, name));
+        SendToServer(handShakeMesage.Serialize());
     }
 
-    void AddClient(IPEndPoint ip, int newClientID)
+    public void AddClient(IPEndPoint ip, int newClientID, string clientName)
     {
         if (!ipToId.ContainsKey(ip) && !clients.ContainsKey(newClientID))
         {
             Debug.Log("Adding client: " + ip.Address);
 
             ipToId[ip] = newClientID;
+            clients.Add(newClientID, new Client(ip, newClientID, Time.realtimeSinceStartup, clientName));
 
-            clients.Add(serverClientID, new Client(ip, newClientID, Time.realtimeSinceStartup));
+            checkActivity.AddClientForList(newClientID);
 
             if (isServer)
             {
-                //Send message to notify the other players that a new one has joined
+                List<(int, string)> playersInServer = new();
+
+                foreach (int id in clients.Keys)
+                {
+                    playersInServer.Add((clients[id].id, clients[id].clientName));
+                }
+
+                ServerToClientHandShake serverToClient = new(playersInServer);
+                Broadcast(serverToClient.Serialize());
             }
         }
         else
         {
-            Debug.Log("This client already exists!");
+            Debug.Log("Es un cliente repetido");
         }
     }
 
-    void RemoveClient(IPEndPoint ip)
+    public void RemoveClient(int idToRemove)
     {
-        if (ipToId.ContainsKey(ip))
+        if (clients.ContainsKey(idToRemove))
         {
-            Debug.Log("Removing client: " + ip.Address);
-            clients.Remove(ipToId[ip]);
+            Debug.Log("Removing client: " + idToRemove);
+
+            checkActivity.RemoveClientForList(idToRemove);
+
+            ipToId.Remove(clients[idToRemove].ipEndPoint);
+            players.Remove(idToRemove);
+            clients.Remove(idToRemove);
         }
     }
 
@@ -108,30 +141,94 @@ public class NetworkManager : MonoBehaviourSingleton<NetworkManager>, IReceiveDa
     {
         switch (messageChecker.CheckMessageType(data))
         {
-            case MessageType.CheckActivity:
+            case MessageType.Ping:
+
+                if (isServer)
+                {
+                    if (ipToId.ContainsKey(ip))
+                    {
+                        checkActivity.ReciveClientToServerPingMessage(ipToId[ip]);
+                    }
+                    else
+                    {
+                        Debug.LogError("Fail Client ID");
+                    }
+                }
+                else
+                {
+                    checkActivity.ReciveServerToClientPingMessage();
+                }
+
                 break;
-            case MessageType.SetClientID:
-                NetSetClientID netGetClientID = new NetSetClientID(data);
-                actualClientID = netGetClientID.GetData();
-                AddClient(ip, actualClientID);
-                Debug.Log("Client's number: " + actualClientID);
+
+            case MessageType.ServerToClientHandShake:
+
+                ServerToClientHandShake netGetClientID = new(data);
+
+                List<(int clientId, string userName)> playerList = netGetClientID.GetData();
+
+                players.Clear();
+                for (int i = 0; i < playerList.Count; i++)
+                {
+                    if (playerList[i].userName == userName)
+                    {
+                        actualClientId = playerList[i].clientId;
+                    }
+
+                    Debug.Log(playerList[i].clientId + " - " + playerList[i].userName);
+                    Player playerToAdd = new Player(playerList[i].clientId, playerList[i].userName);
+                    players.Add(playerList[i].clientId, playerToAdd);
+                }
+
                 break;
-            case MessageType.HandShake:
-                ConnectToServer(data, ip);
+
+            case MessageType.ClientToServerHandShake:
+
+                ReciveClientToServerHandShake(data, ip);
+
                 break;
             case MessageType.Console:
-                UpdateChatText(data);
+
+                UpdateChatText(data, ip);
                 break;
+
             case MessageType.Position:
                 break;
+            case MessageType.NewCustomerNotice:
+                break;
             case MessageType.Disconnection:
+
+                NetDisconnection netDisconnection = new(data);
+
+                int playerID = netDisconnection.GetData();
+                if (isServer)
+                {
+                    Broadcast(data);
+                    RemoveClient(playerID);
+                }
+                else
+                {
+                    Debug.Log("Remove player " + playerID);
+                    RemoveClient(playerID);
+                }
+
+                break;
+            case MessageType.ThereIsNoPlace:
+                break;
+            case MessageType.RepeatMessage:
+                break;
+            case MessageType.Error:
+
+                NetErrorMessage netErrorMessage = new NetErrorMessage(data);
+
+                //NetworkScreen.Instance.SwitchToMenuScreen();
+                //NetworkScreen.Instance.ShowErrorPanel(netErrorMessage.GetData());
+                connection.Close();
+
                 break;
             default:
                 break;
         }
-
-        if (OnReceiveEvent != null)
-            OnReceiveEvent.Invoke(data, ip);
     }
 
     public void SendToServer(byte[] data)
@@ -144,7 +241,7 @@ public class NetworkManager : MonoBehaviourSingleton<NetworkManager>, IReceiveDa
         connection.Send(data, ip);
     }
 
-    public void Broadcast(byte[] data)//Dar rt a los mensajes
+    public void Broadcast(byte[] data)
     {
         using (var iterator = clients.GetEnumerator())
         {
@@ -159,59 +256,104 @@ public class NetworkManager : MonoBehaviourSingleton<NetworkManager>, IReceiveDa
     {
         // Flush the data in main thread
         if (connection != null)
+        {
             connection.FlushReceiveData();
-    }
 
-    void ConnectToServer(byte[]data, IPEndPoint ip)
-    {
-        NetHandShake netHandShake = new NetHandShake(data);
-
-        if (!clients.ContainsKey(serverClientID))
-        {
-            //Assigns an ID to a client and then broadcasts it
-            NetSetClientID netSetClientID = new NetSetClientID(data);
-            Broadcast(netSetClientID.Serialize(), ip);
-
-            AddClient(ip, serverClientID);
-            serverClientID++;
+            if (checkActivity != null)
+            {
+                checkActivity.UpdateCheckActivity();
+            }
         }
     }
 
-    private void UpdateChatText(byte[] data)
+    void ReciveClientToServerHandShake(byte[] data, IPEndPoint ip)
     {
-        int netMessageSum = 0;
-        int sum = 0;
-        char[] aux;
-        string text = "";
+        ClientToServerNetHandShake handShake = new(data);
 
-        NetConsole.Deserialize(data, out aux, out netMessageSum);
-
-        for (int i = 0; i < aux.Length; i++)
+        if (CheckValidUserName(handShake.GetData().Item3, ip) && !ServerIsFull(ip))
         {
-            sum += aux[i];
+            AddClient(ip, serverClientId, handShake.GetData().Item3);
+            serverClientId++;
         }
 
-        Debug.Log(sum);
+    }
 
-        if (sum != netMessageSum)
+    bool ServerIsFull(IPEndPoint ip)
+    {
+        bool serverIsFull = clients.Count >= maxPlayersPerServer;
+
+        if (serverIsFull)
         {
-            //Ask the message again
-            Debug.Log("The message got corrupt");
-            return;
+            NetErrorMessage netServerIsFull = new("Server is full");
+            Broadcast(netServerIsFull.Serialize(), ip);
         }
+
+        return serverIsFull;
+    }
+
+    bool CheckValidUserName(string userName, IPEndPoint ip)
+    {
+        foreach (int clientID in clients.Keys)
+        {
+            if (userName == clients[clientID].clientName)
+            {
+                NetErrorMessage netInvalidUserName = new("Invalid User Name");
+                Broadcast(netInvalidUserName.Serialize(), ip);
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void UpdateChatText(byte[] data, IPEndPoint ip)
+    {
+        string messageText = "";
+
+        NetMessage netMessage = new(data);
+        messageText += new string(netMessage.GetData());
 
         if (isServer)
         {
             Broadcast(data);
         }
 
-        for (int i = 0; i < aux.Length; i++)
+        ChatScreen.Instance.messages.text += messageText + System.Environment.NewLine;
+    }
+
+    void OnApplicationQuit()
+    {
+        if (!isServer)
         {
-            text += aux[i];
+            NetDisconnection netDisconnection = new(actualClientId);
+            SendToServer(netDisconnection.Serialize());
         }
+        else
+        {
+            NetErrorMessage netErrorMessage = new("Lost Connection To Server");
+            CloseServer();
+        }
+    }
 
-        Debug.Log("Message is:" + text);
+    public void CloseServer()
+    {
+        if (isServer)
+        {
+            foreach (int clientId in clients.Keys)
+            {
+                NetDisconnection netDisconnection = new(clientId);
+                Broadcast(netDisconnection.Serialize());
+                RemoveClient(clientId);
+            }
+        }
+    }
 
-        ChatScreen.Instance.messages.text += text + System.Environment.NewLine;
+    public void DisconectPlayer()
+    {
+        if (!isServer)
+        {
+            connection.Close();
+        }
     }
 }

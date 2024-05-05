7f416edfed4041ec28879d38b9d319af7b317b3f
nicorm2000 <nicorm2000@gmail.com>
2024-05-05 14:37:09 -0300

    Updated Message Checker

--- Assets/Scripts/Network/MessageChecker.cs
+++ Assets/Scripts/Network/MessageChecker.cs
@@ -1,22 +1,60 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class MessageChecker
 {
     public MessageType CheckMessageType(byte[] message)
     {
-        int messageType = 0;
-
-        messageType = BitConverter.ToInt32(message, 0);
-
+        int messageType = BitConverter.ToInt32(message, 0);
         return (MessageType)messageType;
     }
 
-    public int CheckClientID(byte[] message)
+    public static byte[] SerializeString(char[] charArray)
     {
-        int clientID = 0;
+        List<byte> outData = new List<byte>();
 
-        clientID = BitConverter.ToInt32(message, 4);
+        outData.AddRange(BitConverter.GetBytes(charArray.Length));
 
-        return clientID;
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            outData.AddRange(BitConverter.GetBytes(charArray[i]));
+        }
+
+        return outData.ToArray();
+    }
+
+    public static string DeserializeString(byte[] message, int indexToInit)
+    {
+        int stringSize = BitConverter.ToInt32(message, indexToInit);
+
+        char[] charArray = new char[stringSize];
+
+        indexToInit += sizeof(int);
+        for (int i = 0; i < stringSize; i++)
+        {
+            charArray[i] = BitConverter.ToChar(message, indexToInit + sizeof(char) * i);
+            Debug.Log(charArray[i]);
+        }
+
+        return new string(charArray);
+    }
+
+    public static bool DeserializeCheckSum(byte[] message)
+    {
+        int messageSum = BitConverter.ToInt32(message, message.Length - sizeof(int));
+
+        messageSum >>= 5;
+
+        return messageSum == message.Length;
+    }
+
+    public static byte[] SerializeCheckSum(List<byte> data)
+    {
+        int sum = data.Count + sizeof(int);
+
+        sum <<= 5;
+
+        return BitConverter.GetBytes(sum);
     }
 }
